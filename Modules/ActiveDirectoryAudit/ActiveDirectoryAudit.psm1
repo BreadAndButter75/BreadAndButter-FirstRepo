dc816fa18bd93b38bf7018c12a44fd9b0914c4d8
Henderson <ub00648@dow.com>
2024-07-08 14:15:58 -0400

    Add ADAudit updates

--- Modules/ActiveDirectoryAudit/ActiveDirectoryAudit.psm1
+++ Modules/ActiveDirectoryAudit/ActiveDirectoryAudit.psm1
@@ -5,10 +5,9 @@ Function Translate-DNToOU {
     )
 
     Try {
-        If($DistinguishedName -Like "OU=*"){
-            $OrganizationalUnit = $DistinguishedName
-        }Else{
-            $OrganizationalUnit = $DistinguishedName.Replace($DistinguishedName.Split(',')[0] + ',','')
+        $OrganizationalUnit = $DistinguishedName
+        While(($OrganizationalUnit -notlike "OU=*") -and ($OrganizationalUnit -notlike "CN=Builtin,*") -and ($OrganizationalUnit -notlike "CN=Microsoft Exchange System Objects,*") -and ($OrganizationalUnit -notlike "CN=Computers,*") -and ($OrganizationalUnit -notlike "ForeignSecurityPrincipals,*") -and ($OrganizationalUnit -notlike "CN=LostAndFound,*") -and ($OrganizationalUnit -notlike "CN=Managed Service Accounts,*") -and ($OrganizationalUnit -notlike "CN=OpsMgrLatencyMonitors,*") -and ($OrganizationalUnit -notlike "CN=Program Data,*") -and ($OrganizationalUnit -notlike "CN=QuestReplicationMonitoring,*") -and ($OrganizationalUnit -notlike "CN=System,*") -and ($OrganizationalUnit -notlike "CN=Unity,*") -and ($OrganizationalUnit -notlike "CN=Users,*") -and ($OrganizationalUnit -notlike "CN=NTDS Quotas,*") -and ($OrganizationalUnit -notlike "CN=TPM Devices")){
+            $OrganizationalUnit = $OrganizationalUnit.Replace($OrganizationalUnit.Split(',')[0] + ',','')
         }
         return $OrganizationalUnit
     }
@@ -20,14 +19,33 @@ Function Translate-DNToOU {
 
 }
 Function Get-OUsWithUsers {
+    Param (
+        [Parameter()]
+        [string]$Domain
+    )
+
     $Global:OUHash = @{}
-    Get-ADUser -Filter * | Foreach-Object {
+    If($Domain){
+        $Params = @{
+            'Filter' = '*'
+            'Server' = $Domain
+        }
+    }Else{
+        $Params = @{
+            'Filter' = '*'
+        }
+    }
+    Get-ADUser @Params | Foreach-Object {
+        Write-Host $_.SamAccountName -ForegroundColor Yellow
         $OUName = Translate-DNToOU $_.DistinguishedName
         If($OUHash.ContainsKey($OUName)){
-            $OUHash.$OUName++
+            ($OUHash.$OUName)++
         }ElseIf(!($OUHash.ContainsKey($OUName))){
             $OUHash.Add($OUName,1)
         }
     }
-    return $Global:$OUHash
+    return $Global:OUHash
 }
+
+Export-ModuleMember Translate-DNtoOU
+Export-ModuleMember Get-OUsWithUsers
